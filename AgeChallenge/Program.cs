3e40936ecdd253ab8b67a3243f00d9688c0fd4f0
Eliana Beltran Feliz <elianabeltranfeliz@gmail.com>
2022-03-11 09:35:37 -0400

    Initial commit

---
+++ AgeChallenge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentYear = 2022;
+
+            Console.WriteLine("¿Qué edad tienes?");
+            Int32 age = 0;
+            Int32.TryParse(Console.ReadLine(), out age);
+            int x = currentYear - age;
+            Console.WriteLine("Naciste en el año " + x);
+            Console.ReadKey();
+        }
+    }
+}
